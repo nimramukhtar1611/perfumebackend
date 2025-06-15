28a62e530bf9e69035020c9132d5a584f384c334
nimra <snimramukhtar321@gmail.com>
2025-06-15 19:50:47 +0500

    code updated

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 connection()
 app.use(express.json());
